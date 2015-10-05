3f2b459cfac4c88f030bd2f0ac6b07ba9dd2aa5c
Dmitry <dvpashnev@mail.ru>
2015-10-05 12:08:10 +0300

    Simple game "Fifteens" 15
    This game was developed for course "WinForms" with using C# and WinForms

---
+++ 15/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace _15
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.NewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.button6 = new System.Windows.Forms.Button();
+      this.button7 = new System.Windows.Forms.Button();
+      this.button8 = new System.Windows.Forms.Button();
+      this.button9 = new System.Windows.Forms.Button();
+      this.button10 = new System.Windows.Forms.Button();
+      this.button11 = new System.Windows.Forms.Button();
+      this.button12 = new System.Windows.Forms.Button();
+      this.button13 = new System.Windows.Forms.Button();
+      this.button14 = new System.Windows.Forms.Button();
+      this.button15 = new System.Windows.Forms.Button();
+      this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+      this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+      this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+      this.menuStrip1.SuspendLayout();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.statusStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewToolStripMenuItem,
+            this.ExitToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(254, 24);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // NewToolStripMenuItem
+      // 
+      this.NewToolStripMenuItem.Name = "NewToolStripMenuItem";
+      this.NewToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+      this.NewToolStripMenuItem.Text = "Новая игра";
+      this.NewToolStripMenuItem.Click += new System.EventHandler(this.begin);
+      // 
+      // ExitToolStripMenuItem
+      // 
+      this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+      this.ExitToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+      this.ExitToolStripMenuItem.Text = "Выход";
+      this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this.tableLayoutPanel1.ColumnCount = 4;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+      this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+      this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+      this.tableLayoutPanel1.Controls.Add(this.button4, 3, 0);
+      this.tableLayoutPanel1.Controls.Add(this.button5, 0, 1);
+      this.tableLayoutPanel1.Controls.Add(this.button6, 1, 1);
+      this.tableLayoutPanel1.Controls.Add(this.button7, 2, 1);
+      this.tableLayoutPanel1.Controls.Add(this.button8, 3, 1);
+      this.tableLayoutPanel1.Controls.Add(this.button9, 0, 2);
+      this.tableLayoutPanel1.Controls.Add(this.button10, 1, 2);
+      this.tableLayoutPanel1.Controls.Add(this.button11, 2, 2);
+      this.tableLayoutPanel1.Controls.Add(this.button12, 3, 2);
+      this.tableLayoutPanel1.Controls.Add(this.button13, 0, 3);
+      this.tableLayoutPanel1.Controls.Add(this.button14, 1, 3);
+      this.tableLayoutPanel1.Controls.Add(this.button15, 2, 3);
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(28, 44);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 4;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 187);
+      this.tableLayoutPanel1.TabIndex = 1;
+      // 
+      // button1
+      // 
+      this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button1.Location = new System.Drawing.Point(3, 3);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(44, 40);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button2
+      // 
+      this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button2.Location = new System.Drawing.Point(53, 3);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(44, 40);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "2";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button3
+      // 
+      this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button3.Location = new System.Drawing.Point(103, 3);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(44, 40);
+      this.button3.TabIndex = 2;
+      this.button3.Text = "3";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button4
+      // 
+      this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button4.Location = new System.Drawing.Point(153, 3);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(44, 40);
+      this.button4.TabIndex = 3;
+      this.button4.Text = "4";
+      this.button4.UseVisualStyleBackColor = true;
+      this.button4.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button5
+      // 
+      this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button5.Location = new System.Drawing.Point(3, 49);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(44, 40);
+      this.button5.TabIndex = 4;
+      this.button5.Text = "5";
+      this.button5.UseVisualStyleBackColor = true;
+      this.button5.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button6
+      // 
+      this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button6.Location = new System.Drawing.Point(53, 49);
+      this.button6.Name = "button6";
+      this.button6.Size = new System.Drawing.Size(44, 40);
+      this.button6.TabIndex = 5;
+      this.button6.Text = "6";
+      this.button6.UseVisualStyleBackColor = true;
+      this.button6.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button7
+      // 
+      this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button7.Location = new System.Drawing.Point(103, 49);
+      this.button7.Name = "button7";
+      this.button7.Size = new System.Drawing.Size(44, 40);
+      this.button7.TabIndex = 6;
+      this.button7.Text = "7";
+      this.button7.UseVisualStyleBackColor = true;
+      this.button7.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button8
+      // 
+      this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button8.Location = new System.Drawing.Point(153, 49);
+      this.button8.Name = "button8";
+      this.button8.Size = new System.Drawing.Size(44, 40);
+      this.button8.TabIndex = 7;
+      this.button8.Text = "8";
+      this.button8.UseVisualStyleBackColor = true;
+      this.button8.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button9
+      // 
+      this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button9.Location = new System.Drawing.Point(3, 95);
+      this.button9.Name = "button9";
+      this.button9.Size = new System.Drawing.Size(44, 40);
+      this.button9.TabIndex = 8;
+      this.button9.Text = "9";
+      this.button9.UseVisualStyleBackColor = true;
+      this.button9.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button10
+      // 
+      this.button10.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button10.Location = new System.Drawing.Point(53, 95);
+      this.button10.Name = "button10";
+      this.button10.Size = new System.Drawing.Size(44, 40);
+      this.button10.TabIndex = 9;
+      this.button10.Text = "10";
+      this.button10.UseVisualStyleBackColor = true;
+      this.button10.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button11
+      // 
+      this.button11.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button11.Location = new System.Drawing.Point(103, 95);
+      this.button11.Name = "button11";
+      this.button11.Size = new System.Drawing.Size(44, 40);
+      this.button11.TabIndex = 10;
+      this.button11.Text = "11";
+      this.button11.UseVisualStyleBackColor = true;
+      this.button11.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button12
+      // 
+      this.button12.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button12.Location = new System.Drawing.Point(153, 95);
+      this.button12.Name = "button12";
+      this.button12.Size = new System.Drawing.Size(44, 40);
+      this.button12.TabIndex = 11;
+      this.button12.Text = "12";
+      this.button12.UseVisualStyleBackColor = true;
+      this.button12.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button13
+      // 
+      this.button13.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button13.Location = new System.Drawing.Point(3, 141);
+      this.button13.Name = "button13";
+      this.button13.Size = new System.Drawing.Size(44, 43);
+      this.button13.TabIndex = 12;
+      this.button13.Text = "13";
+      this.button13.UseVisualStyleBackColor = true;
+      this.button13.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button14
+      // 
+      this.button14.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button14.Location = new System.Drawing.Point(53, 141);
+      this.button14.Name = "button14";
+      this.button14.Size = new System.Drawing.Size(44, 43);
+      this.button14.TabIndex = 13;
+      this.button14.Text = "14";
+      this.button14.UseVisualStyleBackColor = true;
+      this.button14.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // button15
+      // 
+      this.button15.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.button15.Location = new System.Drawing.Point(103, 141);
+      this.button15.Name = "button15";
+      this.button15.Size = new System.Drawing.Size(44, 43);
+      this.button15.TabIndex = 14;
+      this.button15.Text = "15";
+      this.button15.UseVisualStyleBackColor = true;
+      this.button15.Click += new System.EventHandler(this.button15_Click);
+      // 
+      // statusStrip1
+      // 
+      this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+      this.statusStrip1.Location = new System.Drawing.Point(0, 250);
+      this.statusStrip1.Name = "statusStrip1";
+      this.statusStrip1.Size = new System.Drawing.Size(254, 22);
+      this.statusStrip1.TabIndex = 2;
+      this.statusStrip1.Text = "statusStrip1";
+      // 
+      // toolStripProgressBar1
+      // 
+      this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+      this.toolStripProgressBar1.Size = new System.Drawing.Size(150, 16);
+      this.toolStripProgressBar1.Step = 1;
+      // 
+      // toolStripStatusLabel1
+      // 
+      this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+      this.toolStripStatusLabel1.Size = new System.Drawing.Size(43, 17);
+      this.toolStripStatusLabel1.Text = "0 из 15";
+      // 
+      // Form1
+      // 
+      this.AllowDrop = true;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(254, 272);
+      this.Controls.Add(this.statusStrip1);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Controls.Add(this.menuStrip1);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Пятнашки";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.statusStrip1.ResumeLayout(false);
+      this.statusStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem NewToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Button button6;
+    private System.Windows.Forms.Button button7;
+    private System.Windows.Forms.Button button8;
+    private System.Windows.Forms.Button button9;
+    private System.Windows.Forms.Button button10;
+    private System.Windows.Forms.Button button11;
+    private System.Windows.Forms.Button button12;
+    private System.Windows.Forms.Button button13;
+    private System.Windows.Forms.Button button14;
+    private System.Windows.Forms.Button button15;
+    private System.Windows.Forms.StatusStrip statusStrip1;
+    private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+    private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+  }
+}
+
---
+++ 15/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _15
+{
+  public partial class Form1 : Form
+  {
+    private int[,] positions = {{1,5,9,13}, {2,6,10,14}, {3,7,11,15}, {4,8,12,0} };
+    public Form1()
+    {
+      InitializeComponent();
+      begin(NewToolStripMenuItem, new EventArgs());
+    }
+
+    private void begin(object sender, EventArgs e)
+    {
+      foreach (Control c in tableLayoutPanel1.Controls)
+      {
+        if (c is Button)
+        {
+          int col = new Random().Next(0, 3);
+          Thread.Sleep(2);
+          int row = new Random().Next(0, 3);
+          if (tableLayoutPanel1.GetControlFromPosition(col, row) == null)
+          {
+            tableLayoutPanel1.SetCellPosition(c, new TableLayoutPanelCellPosition(col, row));
+          }
+          else
+          {
+            tableLayoutPanel1.SetCellPosition(tableLayoutPanel1.GetControlFromPosition(col, row),
+              tableLayoutPanel1.GetPositionFromControl(c));
+            tableLayoutPanel1.SetCellPosition(c, new TableLayoutPanelCellPosition(col, row));
+          }
+        }
+      }
+    }
+
+    private void button15_Click(object sender, EventArgs e)
+    {
+      Button btn = sender as Button;
+
+      if (tableLayoutPanel1.GetCellPosition(btn).Column + 1 < 4 &&
+        tableLayoutPanel1.GetControlFromPosition(
+        tableLayoutPanel1.GetCellPosition(btn).Column + 1,
+        tableLayoutPanel1.GetCellPosition(btn).Row
+        ) == null)
+      {
+        tableLayoutPanel1.SetCellPosition(btn,
+          new TableLayoutPanelCellPosition(
+            tableLayoutPanel1.GetCellPosition(btn).Column + 1,
+            tableLayoutPanel1.GetCellPosition(btn).Row));
+      }
+      else if (tableLayoutPanel1.GetCellPosition(btn).Column - 1 >= 0 &&
+        tableLayoutPanel1.GetControlFromPosition(
+        tableLayoutPanel1.GetCellPosition(btn).Column - 1,
+        tableLayoutPanel1.GetCellPosition(btn).Row
+        ) == null)
+      {
+        tableLayoutPanel1.SetCellPosition(btn,
+          new TableLayoutPanelCellPosition(
+            tableLayoutPanel1.GetCellPosition(btn).Column - 1,
+            tableLayoutPanel1.GetCellPosition(btn).Row));
+      }
+      else if (tableLayoutPanel1.GetCellPosition(btn).Row + 1 < 4 &&
+        tableLayoutPanel1.GetControlFromPosition(
+        tableLayoutPanel1.GetCellPosition(btn).Column,
+        tableLayoutPanel1.GetCellPosition(btn).Row + 1
+        ) == null)
+      {
+        tableLayoutPanel1.SetCellPosition(btn,
+          new TableLayoutPanelCellPosition(
+            tableLayoutPanel1.GetCellPosition(btn).Column,
+            tableLayoutPanel1.GetCellPosition(btn).Row + 1));
+      }
+      else if (tableLayoutPanel1.GetCellPosition(btn).Row - 1 >= 0 &&
+        tableLayoutPanel1.GetControlFromPosition(
+        tableLayoutPanel1.GetCellPosition(btn).Column,
+        tableLayoutPanel1.GetCellPosition(btn).Row - 1
+        ) == null)
+      {
+        tableLayoutPanel1.SetCellPosition(btn,
+          new TableLayoutPanelCellPosition(
+            tableLayoutPanel1.GetCellPosition(btn).Column,
+            tableLayoutPanel1.GetCellPosition(btn).Row - 1));
+      }
+      check();
+    }
+
+    private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+
+    private void check()
+    {
+      int rezult = 0;
+      for (int i = 0; i < 4; i++ )
+      {
+        for (int j = 0; j < 4; j++)
+        {
+          if(tableLayoutPanel1.GetControlFromPosition(i, j)==null)
+          {
+            continue;
+          }
+          if (Convert.ToInt32(tableLayoutPanel1.GetControlFromPosition(i, j).Text) == positions[i, j])
+          {
+            rezult++;
+          }
+        }
+      }
+      toolStripProgressBar1.Value = (int)(((double)rezult / 15) * 100);
+      toolStripStatusLabel1.Text = rezult.ToString() + " из 15";
+      if (rezult == 15)
+      {
+        MessageBox.Show("Вы выиграли!");
+      }
+    }
+  }
+}
